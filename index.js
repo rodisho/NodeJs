22c698331ad750ba8ebaf6c9c041c9cca7a53dc4
rodisho <rafiodisho@gmail.com>
2020-09-03 22:20:12 -0400

    Build simple api

--- index.js
+++ index.js
@@ -31,6 +31,10 @@ const url = require('url');
 ////////////////////////////////////////////
 ////////// SERVER  /////////////////////////
 
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObject = JSON.parse(data)
+
+
 
 const server = http.createServer((req, res) => {
     const pathName = req.url;
@@ -39,7 +43,12 @@ const server = http.createServer((req, res) => {
         res.end("This is the OVERVIEW")
     } else if( pathName === "/product"){
         res.end("This is the PRODUCT")
-    } else {
+    } else if(pathName === "/api") {
+
+        res.writeHead(200, {'Content-type': 'application/json'})
+        res.end(data)
+    }
+    else {
         res.writeHead(404, {
             'Content-type': 'text/html',
             'my-own-header': 'hello-world'
