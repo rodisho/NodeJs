752491cf9d00dcccdf738f24941c3bd30ca939b7
rodisho <rafiodisho@gmail.com>
2020-09-05 14:41:50 -0400

    Complete filling templates for overview page

--- index.js
+++ index.js
@@ -30,25 +30,42 @@ const url = require('url');
 
 ////////////////////////////////////////////
 ////////// SERVER  /////////////////////////
+    const replaceTemplate = (temp, product) => {
+        let output = temp.replace(/{%PRODUCTNAME%}/g, product.productNamde);
+        output = output.replace(/{%IMAGE%}/g, product.image)
+        output = output.replace(/{%PRICE%}/g, product.price)
+        output = output.replace(/{%FROM%}/g, product.from)
+        output = output.replace(/{%NUTRIENTS%}/g, product.nutrients)
+        output = output.replace(/{%QUANTITY%}/g, product.quantity)
+        output = output.replace(/{%DESCRIPTION%}/g, product.description)
+        output = output.replace(/{%ID%}/g, product.id)
+
+        if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic')
+        return output;
+}
 
 const templateOverview = fs.readFileSync('./templates/template-overview.html', 'utf-8');
 const templateCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
 const templateProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
-const dataObject = JSON.parse(data)
+const dataObject = JSON.parse(data) 
 
 
 
 const server = http.createServer((req, res) => {
     const pathName = req.url;
 
-
     // Overview page
     if(pathName === "/" || pathName === "/overview") {
-
         res.writeHead(200, {'Content-type': 'text/html'})
-        res.end(templateOverview)
+        const cardsHtml = dataObject.map(el => replaceTemplate(templateCard,el)).join('')
+        const output = templateOverview.replace('{%PRODUCT_CARDS%}', cardsHtml)
+        
+        // console.log("CARDSHTML ", cardsHtml)
+
+        // res.end(templateOverview)
+        res.end(output)
 
     // Product page
     } else if( pathName === "/product"){
